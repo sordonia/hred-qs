a98156bda3e6e091cf6a59116416ee17fe2bbc9e
Alessandro Sordoni <sordonia@iro.umontreal.ca>
2015-04-28 16:34:08 -0400

    New orthogonal init (from blocks)

--- utils.py
+++ utils.py
@@ -129,12 +129,34 @@ class Maxout(object):
 def UniformInit(rng, sizeX, sizeY, lb=-0.01, ub=0.01):
     """ Uniform Init """
     return rng.uniform(size=(sizeX, sizeY), low=lb, high=ub).astype(theano.config.floatX)
- 
-def OrthogonalInit(rng, sizeX, sizeY, sparsity=-1, scale=1):
-    """ 
-    Orthogonal Initialization
-    """
 
+def OrthogonalInit(rng, shape):
+    if len(shape) != 2:
+        raise ValueError
+
+    if shape[0] == shape[1]:
+        # For square weight matrices we can simplify the logic
+        # and be more exact:
+        M = rng.randn(*shape).astype(theano.config.floatX)
+        Q, R = numpy.linalg.qr(M)
+        Q = Q * numpy.sign(numpy.diag(R))
+        return Q
+
+    M1 = rng.randn(shape[0], shape[0]).astype(theano.config.floatX)
+    M2 = rng.randn(shape[1], shape[1]).astype(theano.config.floatX)
+
+    # QR decomposition of matrix with entries in N(0, 1) is random
+    Q1, R1 = numpy.linalg.qr(M1)
+    Q2, R2 = numpy.linalg.qr(M2)
+    # Correct that NumPy doesn't force diagonal of R to be non-negative
+    Q1 = Q1 * numpy.sign(numpy.diag(R1))
+    Q2 = Q2 * numpy.sign(numpy.diag(R2))
+
+    n_min = min(shape[0], shape[1])
+    return numpy.dot(Q1[:, :n_min], Q2[:n_min, :])
+
+"""
+def OrthogonalInit(rng, sizeX, sizeY, sparsity=-1, scale=1):
     sizeX = int(sizeX)
     sizeY = int(sizeY)
 
