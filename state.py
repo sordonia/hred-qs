f40d2935dfcf2d36beb4366c3fe4e5f52b4bd648
Alessandro Sordoni <sordonia@iro.umontreal.ca>
2015-04-28 18:42:44 -0400

    for caglar

--- state.py
+++ state.py
@@ -82,46 +82,6 @@ def prototype_state():
     state['minerr'] = -1
     return state
 
-def prototype_rfp_50k():
-    state = prototype_state()
-
-    state['train_session'] = "/part/01/Tmp/sordonia/Work/Reformulations/Datasets/rfp.train.clean_50k.train.ses.pkl"
-    state['train_rank'] = "/part/01/Tmp/sordonia/Work/Reformulations/Datasets/rfp.train.clean_50k.train.rnk.pkl"
-    state['valid_session'] = "/part/01/Tmp/sordonia/Work/Reformulations/Datasets/rfp.train.clean_50k.valid.ses.pkl"
-    state['valid_rank'] = "/part/01/Tmp/sordonia/Work/Reformulations/Datasets/rfp.train.clean_50k.valid.rnk.pkl"
-    state['dictionary'] = "/part/01/Tmp/sordonia/Work/Reformulations/Datasets/rfp.train.clean_50k.dict.pkl"
-     
-    state['reset_recurrence'] = True
-    state['prefix'] = "rfp_30min_50k_"
-    
-    state['lambda_rank'] = 1
-    state['deep_out'] = True
-    state['maxout_out'] = False 
-    state['updater'] = 'adam'
-
-    #
-    state['qdim'] = 600
-    # Dimensionality of session hidden layer 
-    state['sdim'] = 1200 
-    # Dimensionality of low-rank approximation
-    state['rankdim'] = 300
-    # 
-
-    state['bs'] = 80
-    state['seqlen'] = 50
-    
-    state['soq_sym'] = 1
-    state['eoq_sym'] = 2
-    state['eos_sym'] = 3
-    
-    state['save_dir'] = '/part/01/Tmp/sordonia/Work/Reformulations/Datasets/Models/'
-    state['session_rec_activation'] = "lambda x: T.tanh(x)"
-    state['query_rec_activation'] = "lambda x: T.tanh(x)"
-
-    state['session_step_type'] = 'gated'
-    state['query_step_type'] = 'gated' 
-    return state
-
 def prototype_test():
     state = prototype_state()
 
@@ -160,50 +120,184 @@ def prototype_test():
     state['session_rec_activation'] = "lambda x: T.tanh(x)"
     state['query_rec_activation'] = "lambda x: T.tanh(x)"
 
-    state['session_step_type'] = 'gated'
-    state['query_step_type'] = 'gated' 
+    state['session_step_type'] = 'plain'
+    state['query_step_type'] = 'plain' 
     return state
 
-def prototype_aol():
+def aol_bkg_path():
     state = prototype_state()
+    state['dictionary'] = "/part/01/Tmp/sordonia/Work/Reformulations/datasets_cikm/aol.clean.bkg.dict.pkl"
+    state['train_session'] = "/part/01/Tmp/sordonia/Work/Reformulations/datasets_cikm/aol.clean.bkg.ses.pkl"
+    state['train_rank'] = "/part/01/Tmp/sordonia/Work/Reformulations/datasets_cikm/aol.clean.bkg.rnk.pkl"
+    state['valid_session'] = "/part/01/Tmp/sordonia/Work/Reformulations/datasets_cikm/aol.clean.val.ses.pkl"
+    state['valid_rank'] = "/part/01/Tmp/sordonia/Work/Reformulations/datasets_cikm/aol.clean.val.rnk.pkl"
+    return state
 
+def aol_all_path():
+    state = prototype_state()
     state['dictionary'] = "/part/01/Tmp/sordonia/Work/Reformulations/datasets_cikm/aol.train.clean.min_freq_5.dict.pkl"
-    
     state['train_session'] = "/part/01/Tmp/sordonia/Work/Reformulations/datasets_cikm/aol.train.clean.min_freq_5.ses.pkl"
     state['train_rank'] = "/part/01/Tmp/sordonia/Work/Reformulations/datasets_cikm/aol.train.clean.min_freq_5.rnk.pkl"
+    state['valid_session'] = "/part/01/Tmp/sordonia/Work/Reformulations/datasets_cikm/aol.valid.clean.min_freq_5.ses.pkl"
+    state['valid_rank'] = "/part/01/Tmp/sordonia/Work/Reformulations/datasets_cikm/aol.valid.clean.min_freq_5.rnk.pkl"
+    return state
+
+def aol_tf100_50k_path():
+    state = prototype_state()
+    state['dictionary'] = "/part/01/Tmp/sordonia/Work/Reformulations/datasets_cikm/tf100/aol.clean.tf100.cutoff_50k.train.dict.pkl"
+    state['train_session'] = "/part/01/Tmp/sordonia/Work/Reformulations/datasets_cikm/tf100/aol.clean.tf100.cutoff_50k.train.ses.pkl"
+    state['train_rank'] = "/part/01/Tmp/sordonia/Work/Reformulations/datasets_cikm/tf100/aol.clean.tf100.cutoff_50k.train.rnk.pkl"
+    state['valid_session'] = "/part/01/Tmp/sordonia/Work/Reformulations/datasets_cikm/tf100/aol.clean.tf100.cutoff_50k.valid.ses.pkl"
+    state['valid_rank'] = "/part/01/Tmp/sordonia/Work/Reformulations/datasets_cikm/tf100/aol.clean.tf100.cutoff_50k.valid.rnk.pkl"
+    return state 
+
+def aol_tf100_path():
+    state = prototype_state()
+    state['dictionary'] = "/part/01/Tmp/sordonia/Work/Reformulations/datasets_cikm/tf100/aol.clean.tf100.min_freq_5.train.dict.pkl"
+    state['train_session'] = "/part/01/Tmp/sordonia/Work/Reformulations/datasets_cikm/tf100/aol.clean.tf100.min_freq_5.train.ses.pkl"
+    state['train_rank'] = "/part/01/Tmp/sordonia/Work/Reformulations/datasets_cikm/tf100/aol.clean.tf100.min_freq_5.train.rnk.pkl"
+    state['valid_session'] = "/part/01/Tmp/sordonia/Work/Reformulations/datasets_cikm/tf100/aol.clean.tf100.min_freq_5.valid.ses.pkl"
+    state['valid_rank'] = "/part/01/Tmp/sordonia/Work/Reformulations/datasets_cikm/tf100/aol.clean.tf100.min_freq_5.valid.rnk.pkl"
+    return state 
+
+def prototype_aol_bkg():
+    state = aol_bkg_path()
+     
+    state['maxout_out'] = False 
+    state['deep_out'] = True 
+    state['use_nce'] = False 
+     
+    state['updater'] = 'adam'
+    state['lambda_rank'] = 0.01
+    state['valid_freq'] = 5000
+
+    state['qdim'] = 1000
+    # Dimensionality of session hidden layer 
+    state['sdim'] = 1500
+    # Dimensionality of low-rank approximation
+    state['rankdim'] = 300 
+     
+    state['bs'] = 80
+    state['seqlen'] = 50
+     
+    state['eos_sym'] = 2
+    state['eoq_sym'] = 1
+    state['soq_sym'] = -1
+
+    state['save_dir'] = '/part/01/Tmp/sordonia/Work/Reformulations/datasets_cikm/models/' 
+    state['session_rec_activation'] = "lambda x: T.tanh(x)"
+    state['query_rec_activation'] = "lambda x: T.tanh(x)"
+     
+    state['session_step_type'] = "gated"
+    state['query_step_type'] = "gated"
     
-    state['valid_session'] = "/part/01/Tmp/sordonia/Work/Reformulations/datasets_cikm/aol.valid.clean.min_freq_5.pkl"
-    state['valid_rank'] = "/part/01/Tmp/sordonia/Work/Reformulations/datasets_cikm/aol.valid.clean.min_freq_5.pkl"
-        
+    return state
+
+def prototype_aol_bkg_caglar(): 
+    state = prototype_aol_bkg()
+    state['updater'] = 'rmsprop'
+    state['lambda_rank'] = 0.01
+    state['valid_freq'] = 5000
+    state['qdim'] = 512
+    # Dimensionality of session hidden layer 
+    state['sdim'] = 1024
+    # Dimensionality of low-rank approximation
+    state['rankdim'] = 100
+    state['bs'] = 80
+    state['seqlen'] = 50
+     
     state['decoder_bias_type'] = 'all' 
-    state['prefix'] = "aol_min_freq_5"
+    state['prefix'] = "aol_bkg_bias_all"
+    return state
+
+def prototype_aol_bkg_bias_first(): 
+    state = prototype_aol_bkg()
+    state['decoder_bias_type'] = 'first' 
+    state['prefix'] = "aol_bkg_bias_first"
+    return state
+
+def prototype_all_plain():
+    state = all_path()
+     
+    state['decoder_bias_type'] = 'all' 
+    state['prefix'] = 'aol.min_freq_5.gated.bias_all'
      
     state['deep_out'] = True 
     state['maxout_out'] = False 
-    state['use_nce'] = True
-    
+    state['use_nce'] = False 
+     
     state['updater'] = 'adam'
     state['lambda_rank'] = 0.01
-
-    #
+     
+    # Dimensionality of query hidden layer 
     state['qdim'] = 512 
     # Dimensionality of session hidden layer 
     state['sdim'] = 1024 
     # Dimensionality of low-rank approximation
     state['rankdim'] = 256 
-    # 
-
+    
     state['bs'] = 80
     state['seqlen'] = 50
     
     state['eos_sym'] = 3
     state['eoq_sym'] = 2
     state['soq_sym'] = 1
-    
     state['save_dir'] = '/part/01/Tmp/sordonia/Work/Reformulations/datasets_cikm/models/'
+
     state['session_rec_activation'] = "lambda x: T.tanh(x)"
     state['query_rec_activation'] = "lambda x: T.tanh(x)"
-
+     
     state['session_step_type'] = 'gated'
-    state['query_step_type'] = 'gated' 
+    state['query_step_type'] = 'gated'
+    return state
+
+def tf100_nosym_path():
+    state = prototype_state()
+    state['dictionary'] = "/part/01/Tmp/sordonia/Work/Reformulations/splits/tf100/aol.clean.tf100.train.no_sym.min_freq_5.dict.pkl" 
+    state['train_session'] = "/part/01/Tmp/sordonia/Work/Reformulations/splits/tf100/aol.clean.tf100.train.no_sym.min_freq_5.ses.pkl"
+    state['train_rank'] = "/part/01/Tmp/sordonia/Work/Reformulations/splits/tf100/aol.clean.tf100.train.no_sym.min_freq_5.rnk.pkl"
+    state['valid_session'] = "/part/01/Tmp/sordonia/Work/Reformulations/splits/tf100/aol.clean.tf100.valid.no_sym.min_freq_5.ses.pkl"
+    state['valid_rank'] = "/part/01/Tmp/sordonia/Work/Reformulations/splits/tf100/aol.clean.tf100.valid.no_sym.min_freq_5.rnk.pkl"
+    return state
+
+def prototype_tf100_nosym_first():
+    state = tf100_nosym_path()
+     
+    state['decoder_bias_type'] = 'first' 
+    state['prefix'] = 'aol.nosym.min_freq_5.bias_first'
+     
+    state['deep_out'] = True 
+    state['maxout_out'] = False 
+    state['use_nce'] = False 
+     
+    state['updater'] = 'adam'
+    state['lambda_rank'] = 0.01
+     
+    # Dimensionality of query hidden layer 
+    state['qdim'] = 1000 
+    # Dimensionality of session hidden layer 
+    state['sdim'] = 2000 
+    # Dimensionality of low-rank approximation
+    state['rankdim'] = 500 
+    
+    state['bs'] = 80
+    state['seqlen'] = 50
+    
+    state['eos_sym'] = 2
+    state['eoq_sym'] = 1
+    state['soq_sym'] = -1
+    state['save_dir'] = '/part/01/Tmp/sordonia/Work/Reformulations/datasets_cikm/tf100/models'
+
+    state['session_rec_activation'] = "lambda x: T.tanh(x)"
+    state['query_rec_activation'] = "lambda x: T.tanh(x)"
+     
+    state['session_step_type'] = 'gated'
+    state['query_step_type'] = 'gated'
+    return state
+
+def prototype_tf100_50k_first():
+    state = prototype_tf100_50k_all()
+    
+    state['decoder_bias_type'] = 'first'
+    state['prefix'] = 'aol.tf100.cutoff_50k.bias_first'
     return state
